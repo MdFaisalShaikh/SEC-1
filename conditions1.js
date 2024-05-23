cbfdce53b154c8f08e2cdce8ac2f63884c415141
MdFaisalShaikh <faisalshaikh498@gmail.com>
2024-05-23 18:41:15 +0530

    productlist

---
+++ conditions1.js
@@ -0,0 +1,20 @@
+num = 346;
+
+if (num % 2 == 0) {
+    console.log('even');
+
+    x = 'nice';
+    let y = 'good';
+    const pi = 3.14159;
+    // pi = 2635;
+
+
+} else {
+    console.log('odd');
+}
+console.log(x);
+// console.log(y);
+
+
+console.log(1== '1');
+console.log(1=== '1');
---
+++ index1.js
@@ -0,0 +1,12 @@
+console.log('Hello javascript');
+
+a= 2746;
+console.log(a);
+
+weather='Nice';
+
+console.log(weather);
+
+console.log(typeof weather);
+
+console.log(typeof a);
